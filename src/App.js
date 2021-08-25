df2ba4f3284e0c49f06cc53c43b3fc95f8ebdecc
Suvam Chanda <Suvam_Chanda@epam.com>
2021-08-25 14:24:02 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import {Route,Switch} from 'react-router-dom';
+
+
+import AllMeetUpsPage from './pages/AllMeetUps';
+import FavoritesPage from './pages/Favorites';
+import NewMeetUpPage from './pages/NewMeetUp';
+
 function App() {
   return (
     <div >
-      hello world
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetUpsPage/>
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetUpPage/>
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage/>
+        </Route>
+      </Switch>
     </div>
   );
 }
