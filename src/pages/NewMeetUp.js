7414669d71acbc9fa0e7ee9082e2e7c9cf3c0b0b
Suvam Chanda <Suvam_Chanda@epam.com>
2021-10-21 08:44:22 +0530

    Update Code

--- src/pages/NewMeetUp.js
+++ src/pages/NewMeetUp.js
@@ -1,12 +1,28 @@
+import {useHistory} from 'react-router-dom';
+
 import NewMeetUpForm from "../components/meetups/NewMeetUpForm";
 
 function NewMeetUpPage(){
+    const history = useHistory();
+    function addMeetUphandler(meetupData){
+        
+        fetch('https://react-getting-started-6c965-default-rtdb.firebaseio.com/meetups.json',
+        {
+            method:'POST',
+            body:JSON.stringify(meetupData),
+            headers:{
+                'Content-Type' : 'application/json'
+            }
+        }).then(()=>{
+            history.replace('/');
+        });
+    }
     return (
         <section>
             <h1>
                 Add New Meetup
             </h1>
-            <NewMeetUpForm></NewMeetUpForm>
+            <NewMeetUpForm onAddMeetUp={addMeetUphandler}></NewMeetUpForm>
         </section>
     );
 }
